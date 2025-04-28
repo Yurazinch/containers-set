2ac5d9cdc44418edd78d906c609c9a6a56414207
Yuriy Zinchenko <yurazinch@gmail.com>
2025-04-28 21:22:17 +0300

    Не проходит тест

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,39 @@
-import sum from '../basic';
+import Team from '../basic';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+const team = new Team;
+const unit = {name: 'Маг', health: 95};
+const units = [
+  {name: 'Маг', health: 95},
+  {name: 'Лучник', health: 45},
+  {name: 'Мишень', health: 10}
+]
 
-  expect(result).toBe(6);
+team.clear;
+
+test('add hit unit', () => {  
+  team.add(unit);
+  let received = team.size;
+  expect(received).toBe(1);
 });
+
+test('add hit unit 2', () => {  
+  team.add(unit);
+  // eslint-disable-next-line no-undef
+  expect(thrown).toBe('Юнит уже добавлен в команду');
+});
+
+test ('add more units', () => {  
+  team.addAll(...units);
+  let received = team.size;
+  expect(received).toBe(3);
+})
+
+test ('add to array', () => {
+   let received = team.toArray();
+   let expected = [
+    {name: 'Маг', health: 95},
+    {name: 'Лучник', health: 45},
+    {name: 'Мишень', health: 10}
+  ]
+  expect(received).toBe(expected);
+})
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,21 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+const hitUnit = {};
+export default class Team {
+  constructor() {
+      this.members = new Set();
   }
-  return result;
+  add() {
+    if(this.members.has(hitUnit)) {
+      throw 'Юнит уже добавлен в команду';
+    }
+    this.members.add(hitUnit);
+  }
+  addAll(...units) {
+    this.members.add(...units);
+  }
+  toArray() {
+    // eslint-disable-next-line no-unused-vars
+    const array = [];
+    this.members.forEach(item => array.push(item));
+    return array;
+  }
 }
